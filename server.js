362cf2ddec3e6cea2425143d3983118eace08b22
Lukas <lukasborges@gmail.com>
2016-11-18 20:51:18 +0000

    server.js spawns multiple express servers

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var http = require('http');
+var morgan  = require('morgan');
+var http    = require('http');
+var express = require('express');
+var path    = require('path');
+var app		= express();
+
+//for logging
+app.use(morgan('combined'));
+
+//public static file serving
+app.use(express.static('public'));
+
+//passing index.html file
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 function serve(ip, port)
 {
-        http.createServer(function (req, res) {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end("There's no place like "+ip+":"+port+"\n");
-        }).listen(port, ip);
-        console.log('Server running at http://'+ip+':'+port+'/');
+	//http.createServer(app).listen(port, ip);
+	app.listen(port, ip);
+	console.log('Express server is running at http://'+ip+':'+port+'/');
 }
 
 serve('104.131.14.10', 3000);
